56ef89a7540db944463ac8c7740b0e3823c53d1c
Derin-Dehrin <aderinolakenny777@gmail.com>
2022-12-08 15:54:49 -0800

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *My hello , printf task
- *Return: return 1 after program execution
+ *main - prints the phrase with proper grammar,
+ *but the outcome is a piece of art,
+ *Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-printf("with proper grammar , but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar , but the outcome is a piece of art,\n");
+	return (0);
 }
