8c1342359dc0f3efac1d75e3210806485c3e9446
Derin-Dehrin <aderinolakenny777@gmail.com>
2022-12-08 14:59:28 -0800

    Hello ,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *My hello printf task
- *Return: return to 1 after program exexcution
+ *My hello , printf task
+ *Return: return 1 after program execution
  */
 int main(void)
 {
