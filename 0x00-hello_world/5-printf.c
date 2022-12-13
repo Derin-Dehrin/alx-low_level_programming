4e0c0b300b9b4b90583d0b11d873f00fa0b7714d
Derin-Dehrin <aderinolakenny777@gmail.com>
2022-12-12 17:16:55 -0800

    Hello.printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry
- *Return: Always 0
+ *main - Entry point
+ *MY hello, printf task
+ *Return: return 1 after program execution
  */
 int main(void)
 {
-	printf("with proper grammar , but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar , but the outcome is a piece of art,\n");
+return (0);
 }
