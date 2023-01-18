0eb9c95f9b2c69fc216030b70ffa3b174553f84f
Derin-Dehrin <aderinolakenny777@gmail.com>
2023-01-18 05:01:06 -0800

    if you spend too much time thinking

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,9 +7,10 @@
  *
  * Return: void.
  */
-void array_iterator(int array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
 	if (array && size > 0 && action)
 	{
 		for (i = 0; i < size; i++)
