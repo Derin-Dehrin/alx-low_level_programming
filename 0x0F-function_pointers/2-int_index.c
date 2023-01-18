a1cd993f75a45ea712bc5e60b046d74840635a1d
Derin-Dehrin <aderinolakenny777@gmail.com>
2023-01-18 05:43:46 -0800

    to hell with circumstances

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,7 @@
 /**
  * int_index - searches for an integer.
  * @array: array of integers.
+ * @size: number of elements in array.
  * @cmp: pointer to function used to compare values.
  *
  * Return: index of first element.
